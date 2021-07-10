d1aaaf6c6dae8ab9290ea3654b29ccb357339c9f
nisargmankad <nisarg.mankad@gmail.com>
2021-07-10 18:14:06 -0400

    removed test proj

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using System;
+
+namespace HelloWorldTest
+{
+    
+    public class UnitTest1
+    {
+        private const string Expected ="Hello Wolrd!";
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CICD_Demo.Program.Main();
+
+                var result= sw.ToString().Trim();
+                AssemblyLoadEventArgs.Equals(Expected,result);
+            }
+        }
+    }
+}
